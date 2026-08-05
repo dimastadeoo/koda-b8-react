4c2c88fb0d8f20990c59f4c2acbfcdbd3d47acda
dimastadeoo <dimastadeoo@gmail.com>
2026-08-05 13:18:32 +0700

    feat: add productReducer to rootReducer for managing product state

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import userReducer from './userSlice.js';
 import profileReducer from './profileSlice.js';
 import profileDataReducer from './profileDataSlice.js'
+import productReducer from './productSlice.js'
 
 const rootReducer = combineReducers({
   user: userReducer,
   profile: profileReducer,
   profileData: profileDataReducer,
+  products: productReducer,
   // tambahkan reducer lain di sini jika nanti ada
 });
 
