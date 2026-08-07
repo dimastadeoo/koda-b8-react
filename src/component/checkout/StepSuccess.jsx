c4e9beffa4b5892ffc6114c6197c1b6f19fa5a33
dimastadeoo <dimastadeoo@gmail.com>
2026-08-07 10:58:17 +0700

    refactor: update StepSuccess component to utilize order object and enhance order summary display

--- src/component/checkout/StepSuccess.jsx
+++ src/component/checkout/StepSuccess.jsx
@@ -1,16 +1,10 @@
-import { Link, useLocation, useOutletContext } from "react-router";
+import { Link, useOutletContext } from "react-router";
 import { FaBoxOpen, FaCheck, FaCheckCircle, FaTruck } from "react-icons/fa";
-
 import { formatRupiah } from "../CartItem";
 
 export default function StepSuccess() {
-  const location = useLocation();
-  const { checkout, checkoutId } = useOutletContext();
-
-  const activeCheckout =
-    location.state?.checkout?.id === checkoutId
-      ? location.state.checkout
-      : checkout;
+  const { order } = useOutletContext();
+  const total = order.subtotal + order?.shipping_price || 0;
 
 
   return (
@@ -20,111 +14,50 @@ export default function StepSuccess() {
       </div>
 
       <div className="grid gap-2 justify-center items-center">
-        <h1 className="text-center font-bold text-2xl text-[#111827]">
-          Pesanan Berhasil! 🎉
-        </h1>
-        <p className="text-center text-base font-normal text-[#6B7280]">
-          Terima kasih telah berbelanja di BeliMudah. Pesananmu sedang diproses.
-        </p>
+        <h1 className="text-center font-bold text-2xl text-[#111827]">Pesanan Berhasil! 🎉</h1>
+        <p className="text-center text-base font-normal text-[#6B7280]">Terima kasih telah berbelanja di BeliMudah. Pesananmu sedang diproses.</p>
       </div>
 
       <div className="grid gap-4 p-6 border border-black/10 bg-white rounded-2xl">
         <div className="flex justify-between gap-6 max-md:grid max-md:grid-cols-2 max-sm:grid-cols-1">
           <div>
             <p className="font-normal text-sm text-[#6B7280]">Nomor Pesanan</p>
-            <h2 className="font-bold text-base text-[#1A73E8]">
-              #{activeCheckout.id}
-            </h2>
+            <h2 className="font-bold text-base text-[#1A73E8]">#{order.id}</h2>
           </div>
-
           <div>
-            <p className="font-normal text-sm text-[#6B7280]">
-              Total Pembayaran
-            </p>
-            <h1 className="font-bold text-base text-[#111827]">
-              {formatRupiah(activeCheckout.total)}
-            </h1>
+            <p className="font-normal text-sm text-[#6B7280]">Total Pembayaran</p>
+            <h1 className="font-bold text-base text-[#111827]">{formatRupiah(total)}</h1>
           </div>
-
           <div>
-            <h4 className="text-sm font-normal text-[#111827]">
-              {activeCheckout.shipping?.shippingMethod}
-            </h4>
-            <p className="text-xs font-normal text-[#6B7280]">
-              Estimasi tiba: 3-5 hari kerja
-            </p>
+            <h4 className="text-sm font-normal text-[#111827]">{order.shipping_name || 'Pengiriman'}</h4>
+            <p className="text-xs font-normal text-[#6B7280]">Estimasi tiba: 3-5 hari kerja</p>
           </div>
-
           <div>
-            <h4 className="text-sm font-normal text-[#111827]">
-              Alamat Pengiriman
-            </h4>
-            <p className="text-xs font-normal text-[#6B7280]">
-              {activeCheckout.shipping?.alamat}
-            </p>
+            <h4 className="text-sm font-normal text-[#111827]">Alamat Pengiriman</h4>
+            <p className="text-xs font-normal text-[#6B7280]">{order.address}</p>
           </div>
         </div>
       </div>
 
       <div className="grid gap-4 p-6 border border-black/10 bg-white rounded-2xl">
-        <h2 className="text-xl font-medium flex items-center gap-2 text-[#111827]">
-          Status Pesanan
-        </h2>
-
+        <h2 className="text-xl font-medium flex items-center gap-2 text-[#111827]">Status Pesanan</h2>
         <div className="flex gap-4 items-start">
-          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-[#DCFCE7] shrink-0">
-            <FaCheck className="text-[#00A63E]" />
-          </div>
-
-          <div className="flex flex-col">
-            <p className="font-medium text-sm text-[#111827]">
-              Pesanan Diterima
-            </p>
-            <p className="text-xs text-[#6B7280]">Baru saja</p>
-          </div>
+          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-[#DCFCE7] shrink-0"><FaCheck className="text-[#00A63E]" /></div>
+          <div><p className="font-medium text-sm text-[#111827]">Pesanan Diterima</p><p className="text-xs text-[#6B7280]">Baru saja</p></div>
         </div>
-
         <div className="flex gap-4 items-start">
-          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-[#E5E7EB] shrink-0">
-            <FaBoxOpen className="text-[#6B7280]" />
-          </div>
-
-          <div className="flex flex-col">
-            <p className="font-medium text-sm text-[#111827]">
-              Sedang Dikemas
-            </p>
-            <p className="text-xs text-[#6B7280]">Estimasi 1-2 jam</p>
-          </div>
+          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-[#E5E7EB] shrink-0"><FaBoxOpen className="text-[#6B7280]" /></div>
+          <div><p className="font-medium text-sm text-[#111827]">Sedang Dikemas</p><p className="text-xs text-[#6B7280]">Estimasi 1-2 jam</p></div>
         </div>
-
         <div className="flex gap-4 items-start">
-          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-[#E5E7EB] shrink-0">
-            <FaTruck className="text-[#6B7280]" />
-          </div>
-
-          <div className="flex flex-col">
-            <p className="font-medium text-sm text-[#111827]">
-              Dalam Pengiriman
-            </p>
-            <p className="text-xs text-[#6B7280]">3-5 hari kerja</p>
-          </div>
+          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-[#E5E7EB] shrink-0"><FaTruck className="text-[#6B7280]" /></div>
+          <div><p className="font-medium text-sm text-[#111827]">Dalam Pengiriman</p><p className="text-xs text-[#6B7280]">3-5 hari kerja</p></div>
         </div>
       </div>
 
       <div className="flex justify-start items-center gap-3 px-3 max-md:flex-wrap max-sm:flex-col max-sm:items-stretch">
-        <Link
-          to="/profile/my-orders"
-          className="flex justify-center items-center gap-2 bg-green-600 hover:bg-green-800 text-white border-none px-6 py-3 rounded-xl text-base font-normal cursor-pointer"
-        >
-          Lihat Riwayat Pesanan
-        </Link>
-
-        <Link
-          to="/main"
-          className="ml-auto flex items-center justify-center gap-2 text-green-500 no-underline font-normal text-base px-6 py-3 hover:underline max-md:ml-0"
-        >
-          Lanjut Belanja
-        </Link>
+        <Link to="/profile/my-orders" className="flex justify-center items-center gap-2 bg-green-600 hover:bg-green-800 text-white border-none px-6 py-3 rounded-xl text-base font-normal cursor-pointer">Lihat Riwayat Pesanan</Link>
+        <Link to="/main" className="ml-auto flex items-center justify-center gap-2 text-green-500 no-underline font-normal text-base px-6 py-3 hover:underline max-md:ml-0">Lanjut Belanja</Link>
       </div>
     </section>
   );
