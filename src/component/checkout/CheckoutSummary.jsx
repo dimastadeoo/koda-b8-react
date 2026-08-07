b28c471ea3b0547d05cd5840d47aa350d6f15c69
dimastadeoo <dimastadeoo@gmail.com>
2026-08-07 10:57:52 +0700

    refactor: update CheckoutSummary component to use order object and improve item rendering

--- src/component/checkout/CheckoutSummary.jsx
+++ src/component/checkout/CheckoutSummary.jsx
@@ -1,31 +1,26 @@
 import { formatRupiah } from "../CartItem";
+import { getImageProducts } from "../utils/image";
 
-export default function CheckoutSummary({ checkout }) {
+export default function CheckoutSummary({ order }) {
+  // Pastikan order memiliki items
+  const items = order.items || [];
+  console.log(order)
+  const subtotal = order.subtotal || 0;
+  const total = order.subtotal + order?.shipping_price || 0;
+  const shippingPrice = order.shipping_price
 
   return (
     <div className="grid gap-4 p-5 bg-white border border-black/10 rounded-xl">
-      <h3 className="font-medium text-lg text-[#111827]">
-        Ringkasan Pesanan
-      </h3>
+      <h3 className="font-medium text-lg text-[#111827]">Ringkasan Pesanan</h3>
 
       <div className="grid gap-3">
-        {checkout.items.map((item) => (
-          <div key={item.productId} className="flex items-center gap-2">
-            <img
-              src={item.image?.[0]}
-              alt={item.cartNameContent}
-              className="w-10 h-10 object-cover rounded-lg"
-            />
-
+        {items.map((item) => (
+          <div key={item.id_product} className="flex items-center gap-2">
+            <img src={getImageProducts(item.primary_img) || '/img/placeholder.png'} alt={item.product_name_snapshot} className="w-10 h-10 object-cover rounded-lg" />
             <div>
-              <h4 className="font-normal text-xs text-[#6B7280] line-clamp-1">
-                {item.cartNameContent}
-              </h4>
+              <h4 className="font-normal text-xs text-[#6B7280] line-clamp-1">{item.product_name_snapshot}</h4>
             </div>
-
-            <span className="ml-auto text-xs font-normal text-[#111827]">
-              ×{item.quantity}
-            </span>
+            <span className="ml-auto text-xs font-normal text-[#111827]">×{item.qty}</span>
           </div>
         ))}
       </div>
@@ -33,25 +28,21 @@ export default function CheckoutSummary({ checkout }) {
       <div className="grid pt-3 gap-2 border-t border-black/10">
         <div className="flex justify-between text-sm font-normal text-[#6B7280]">
           <span>Subtotal</span>
-          <span>{formatRupiah(checkout.subtotal || checkout.total)}</span>
+          <span>{formatRupiah(subtotal)}</span>
         </div>
-
+        {shippingPrice && (
         <div className="flex justify-between text-sm font-normal text-[#6B7280]">
           <span>Ongkir</span>
-          <span className="text-[#00A63E]">Gratis</span>
+          <span className="text-[#00A63E]">{shippingPrice}</span>
         </div>
-
+        )}
         <div className="flex justify-between border-t border-black/10 pt-2 text-sm font-semibold text-[#1a202c]">
           <span>Total</span>
-          <span className="text-green-600">
-            {formatRupiah(checkout.total)}
-          </span>
+          <span className="text-green-600">{formatRupiah(total)}</span>
         </div>
       </div>
 
-      <p className="text-center text-xs font-normal text-[#6B7280]">
-        🛡️ Pembayaran aman dan terenkripsi
-      </p>
+      <p className="text-center text-xs font-normal text-[#6B7280]">🛡️ Pembayaran aman dan terenkripsi</p>
     </div>
   );
 }
